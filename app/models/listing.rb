9fe0c1de73f04f8f0fe72ee7ffddd488c3903531
Emerson Scherer <emerson@emersonscherer.com>
2015-03-21 22:20:19 -0400

    Added Image Functions in Edit and Show

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ActiveRecord::Base
+  has_attached_file :image,  :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "no_thumb.jpg"
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 end
